f54ac36c9fd83dd7cda2bde8fefc9c404b129ba3
Reyhan Jovie <reyhanjovie01@gmail.com>
2021-05-01 19:13:04 +0700

    change env

--- config/next.config.js
+++ config/next.config.js
@@ -1,5 +1,5 @@
 const config={
-    BASE_URL: `${process.env.NEXT_PUBLIC_BASE_URL +":"+ process.env.NEXT_PUBLIC_BASE_PORT}`,
+    BASE_URL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     TOKEN_MAP : process.env.TOKEN_MAP
 }
 
